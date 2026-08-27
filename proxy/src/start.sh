50ddf70710a0447b3ac57a4132d67fa8b2c3565b
Nikolay Mitrofanov <n.mitrofanow@gmail.com>
2026-08-27 17:53:38 +0300

    ++

--- proxy/src/start.sh
+++ proxy/src/start.sh
@@ -4,71 +4,145 @@
 # License found in the LICENSE file in the root directory
 # of this source tree.
 
-set -e
 
-## Script envs
-# HAPROXY_BIN - path to haproxy binary. Default: haproxy
-# WHATSAPP_PROXY_CONFIG_FILE - path to ha proxy config. Default: /usr/local/etc/haproxy/haproxy.cfg
-# WHATSAPP_PROXY_CERT_FILE - path to proxy tls key and cert. Default: /etc/haproxy/ssl/proxy.whatsapp.net.pem 
-# WHATSAPP_PROXY_CERT_FILE_CHOWN - if passed will chown to cert file to passed string
-
-## Config envs
-# WHATSAPP_STATS_BIND_PORT - if passed bind to 127.0.0.1:${WHATSAPP_STATS_BIND_PORT} metrics server
-# WHATSAPP_V4_HTTP_FRONTEND_BIND - http frontend bind string
-#   Default - 127.0.0.1:80
-# WHATSAPP_V4_HTTP_FRONTEND_ACCEPT_PROXY_BIND - if passed bind frontend with pass real ip for http
-# WHATSAPP_V4_HTTPS_FRONTEND_BIND - http frontend bind string
-#   Default - 127.0.0.1:443
-# WHATSAPP_V4_HTTPS_FRONTEND_ACCEPT_PROXY_BIND - if passed bind frontend with pass real ip for https
-# WHATSAPP_V4_XMPP_FRONTEND_BIND - xmpp frontend bind string
-#   Default - 127.0.0.1:5222
-# WHATSAPP_V4_XMPP_FRONTEND_ACCEPT_PROXY_BIND - if passed bind frontend with pass real ip for xmpp
-# WHATSAPP_V4_NET_FRONTEND_BIND - whatsapp net frontend bind string
-#   Default - 127.0.0.1:7777
-# WHATSAPP_NET_DESTINATION - proxy destination for whatsapp net with tls
-#   Default - whatsapp.net:443
-# WHATSAPP_XMPP_DESTINATION - proxy destination for xmpp
-#   Default - g.whatsapp.net:5222
-# WHATSAPP_NET_HTTP_DESTINATION - proxy destination for whatsapp net without tls
-#   Default - g.whatsapp.net:80
+set -Eeuo pipefail
 
 working_dir=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
-if [ -z "$HAPROXY_BIN" ]; then
+haproxy_cfg_default="${working_dir}/whatsapp-haproxy.cfg"
+
+function echo_err() {
+	echo -e "\033[0;31m${1:-}\033[0m" >&2
+}
+
+function exit_with_err() {
+    local exit_code="${2:-}"
+
+    if [ -z "$exit_code" ]; then
+        exit_code="1"
+    fi
+
+    if [ "$exit_code" -eq 0 ]; then
+        exit_code="1"
+    fi
+
+    echo_err "${1:-Error}"
+    exit "$exit_code"
+}
+
+function usage() {
+    local exit_code="${1-}"
+    if [ -z "$exit_code" ]; then
+      exit_code="0"
+    fi
+    echo "Usage: $0 [OPTIONS]"
+    echo "Options:"
+    echo "  -c|--config PATH - Path to config file with envs params"
+    echo "                     Also can be passed via env WHATSAPP_START_CONFIG_FILE"
+    echo "                     Config file should be dot env file with next variables:"
+    echo "                     Start script envs:"
+    echo "                     - HAPROXY_BIN - path to haproxy binary."
+    echo "                         Optional, default: haproxy"
+    echo "                     - WHATSAPP_PROXY_CONFIG_FILE - path to haproxy config."
+    echo "                         Optional, default: $haproxy_cfg_default"
+    echo "                     - WHATSAPP_PROXY_CERT_FILE - path to proxy tls key and cert."
+    echo "                         Optional, default: /etc/haproxy/ssl/proxy.whatsapp.net.pem"
+    echo "                         If file exists and not empty generating cert will skip."
+    echo "                     - WHATSAPP_PROXY_CERT_FILE_CHOWN - if passed will chown to cert file to passed string"
+    echo "                         Optional."
+    echo "                     "
+    echo "                     Next variables will use for configure proxy in proxy config file:"
+    echo "                     - WHATSAPP_STATS_BIND_PORT - port for bind haproxy prometheus metrics server."
+    echo "                         If passed bind to 127.0.0.1:\${WHATSAPP_STATS_BIND_PORT}"
+    echo "                         Optional."
+    echo "                     - WHATSAPP_V4_HTTP_FRONTEND_BIND - http frontend bind string."
+    echo "                         Optional, default: 127.0.0.1:80"
+    echo "                     - WHATSAPP_V4_HTTP_FRONTEND_ACCEPT_PROXY_BIND - if passed, bind frontend with pass real ip for http."
+    echo "                         Optional."
+    echo "                     - WHATSAPP_V4_HTTPS_FRONTEND_BIND - https (media) frontend bind string"
+    echo "                         Optional, default: 127.0.0.1:443"
+    echo "                     - WHATSAPP_V4_HTTPS_FRONTEND_ACCEPT_PROXY_BIND - if passed bind frontend with pass real ip for https"
+    echo "                         Optional."
+    echo "                     - WHATSAPP_V4_XMPP_FRONTEND_BIND - xmpp frontend bind string"
+    echo "                         Optional, default: 127.0.0.1:5222"
+    echo "                     - WHATSAPP_V4_XMPP_FRONTEND_ACCEPT_PROXY_BIND - if passed bind frontend with pass real ip for xmpp"
+    echo "                         Optional."
+    echo "                     - WHATSAPP_V4_NET_FRONTEND_BIND - whatsapp net (chat) frontend bind string"
+    echo "                         Optional, default: 127.0.0.1:7777"
+    echo "                     - WHATSAPP_NET_DESTINATION - proxy destination for whatsapp net (chat) with tls"
+    echo "                         Optional, default: whatsapp.net:443"
+    echo "                     - WHATSAPP_XMPP_DESTINATION - proxy destination for xmpp"
+    echo "                         Optional, default: g.whatsapp.net:5222"
+    echo "                     - WHATSAPP_NET_HTTP_DESTINATION - proxy destination for whatsapp net (chat) without tls"
+    echo "                         Optional, default: g.whatsapp.net:80"
+    echo "                     "
+    echo "  -h|--help          - Display this help message"
+    exit "$exit_code"
+}
+
+if [ -z "${WHATSAPP_START_CONFIG_FILE:-}" ]; then
+    WHATSAPP_START_CONFIG_FILE=""
+fi
+
+while [[ $# -gt 0 ]]; do
+    case "$1" in
+        -c|--config)
+        if [[ -z "${2:-}" || "${2:-}" == -* ]]; then
+            exit_with_err "Error: Argument for $1 is missing" >&2
+        fi
+        WHATSAPP_START_CONFIG_FILE="${2-}"
+        shift 2
+        ;;
+        -h|--help)
+        usage "0"
+        ;;
+        *)
+        echo_err "Unknown argument '${1}'"
+        usage "1"
+        ;;
+    esac
+done
+
+if [ -n "${WHATSAPP_START_CONFIG_FILE:-}" ]; then
+    if [ ! -f "$WHATSAPP_START_CONFIG_FILE" ]; then
+        exit_with_err "Config file '$WHATSAPP_START_CONFIG_FILE' is not found or not file"
+    fi
+    set -a 
+    # shellcheck disable=SC1090
+    if ! source "$WHATSAPP_START_CONFIG_FILE"; then
+        set +a
+        exit_with_err "Cannot source '$WHATSAPP_START_CONFIG_FILE' with config envs" 
+    fi
+    set +a
+fi
+
+if [ -z "${HAPROXY_BIN:-}" ]; then
     HAPROXY_BIN="haproxy"
 fi
 
 if ! command "$HAPROXY_BIN" > /dev/null; then
-    echo "haproxy '$HAPROXY_BIN' binary not found"
-    exit 1
+    exit_with_err "haproxy '$HAPROXY_BIN' binary not found"
 fi
 
-if [ -z "$WHATSAPP_PROXY_CONFIG_FILE" ]; then
-    export WHATSAPP_PROXY_CONFIG_FILE="/usr/local/etc/haproxy/haproxy.cfg"
-fi
-
-if [ ! -f "$WHATSAPP_PROXY_CONFIG_FILE" ]; then
-    echo "Proxy config file '$WHATSAPP_PROXY_CONFIG_FILE' not found or not file"
-    exit 1
+if [ -z "${WHATSAPP_PROXY_CONFIG_FILE:-}" ]; then
+    export WHATSAPP_PROXY_CONFIG_FILE="$haproxy_cfg_default"
 fi
 
 if [ ! -s "$WHATSAPP_PROXY_CONFIG_FILE" ]; then
-    echo "Proxy config file '$WHATSAPP_PROXY_CONFIG_FILE' is empty"
-    exit 1
+    exit_with_err "Proxy config file '$WHATSAPP_PROXY_CONFIG_FILE' not found or is empty"
 fi
 
-if [ -z "$WHATSAPP_PROXY_CERT_FILE" ]; then
+if [ -z "${WHATSAPP_PROXY_CERT_FILE:-}" ]; then
     WHATSAPP_PROXY_CERT_FILE="/etc/haproxy/ssl/proxy.whatsapp.net.pem"
 fi
 
-if [ ! -s "$WHATSAPP_PROXY_CERT_FILE" ]; then
+if [ ! -s "${WHATSAPP_PROXY_CERT_FILE:-}" ]; then
     echo "Cert file '$WHATSAPP_PROXY_CERT_FILE' is not exists. Generate cert file"
 
     full_gen_cert_script="${working_dir}/generate-certs.sh"
 
     if [ ! -s "$full_gen_cert_script" ]; then
-        echo "Gen cert script script '$full_gen_cert_script' is not found or empty"
-        exit 1
+        exit_with_err "Gen cert script script '$full_gen_cert_script' is not found or empty"
     fi
 
     export tmp_cert_dir=""
@@ -81,13 +155,11 @@ if [ ! -s "$WHATSAPP_PROXY_CERT_FILE" ]; then
     }
 
     if ! tmp_cert_dir="$(mktemp -d)"; then
-        echo "Cannot create temp dir for generating cert"
-        exit 1
+        exit_with_err "Cannot create temp dir for generating cert"
     fi
 
     if [ -z "$tmp_cert_dir" ] || [ ! -d "$tmp_cert_dir" ]; then
-        echo "Temp cert dir '$tmp_cert_dir' is empty or not dir"
-        exit 1
+        exit_with_err "Temp cert dir '$tmp_cert_dir' is empty or not dir"
     fi
 
     trap 'cleanup_tmp_dir' EXIT
@@ -96,18 +168,26 @@ if [ ! -s "$WHATSAPP_PROXY_CERT_FILE" ]; then
 
     pushd "$tmp_cert_dir"
     
-    "$full_gen_cert_script"
+    if ! "$full_gen_cert_script"; then
+        exit_with_err "Cannot generate cert"
+    fi
+
+    if [ ! -s "proxy.whatsapp.net.pem" ]; then
+        exit_with_err "Cert file proxy.whatsapp.net.pem not found or empty"
+    fi
 
     if ! mv proxy.whatsapp.net.pem "$WHATSAPP_PROXY_CERT_FILE"; then
-        echo "Cannot move proxy.whatsapp.net.pem to '$WHATSAPP_PROXY_CERT_FILE'"
-        exit 1
+        exit_with_err "Cannot move proxy.whatsapp.net.pem to '$WHATSAPP_PROXY_CERT_FILE'"
     fi
     
-    if [ -n "$WHATSAPP_PROXY_CERT_FILE_CHOWN" ]; then
+    if [ -n "${WHATSAPP_PROXY_CERT_FILE_CHOWN:-}" ]; then
         if ! chown "$WHATSAPP_PROXY_CERT_FILE_CHOWN" "$WHATSAPP_PROXY_CERT_FILE"; then
-            echo "Cannot chown '$WHATSAPP_PROXY_CERT_FILE' to '$WHATSAPP_PROXY_CERT_FILE_CHOWN'"
-            exit 1
+            exit_with_err "Cannot chown '$WHATSAPP_PROXY_CERT_FILE' to '$WHATSAPP_PROXY_CERT_FILE_CHOWN'"
         fi
+    fi
+
+    if ! chmod 600 "$WHATSAPP_PROXY_CERT_FILE"; then
+        echo_err "Cannot chmod '$WHATSAPP_PROXY_CERT_FILE' to 600. Continue"
     fi
     
     popd
